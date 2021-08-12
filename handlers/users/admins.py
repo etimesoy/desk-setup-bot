13bb950a97753970224c28b1f50a34cfe76ba50e
Ruslan <ruslangazizov36@gmail.com>
2021-08-12 19:10:36 +0300

    Add show_users button realisation in admin panel

--- handlers/users/admins.py
+++ handlers/users/admins.py
@@ -2,9 +2,27 @@ from aiogram import types
 
 from keyboards.inline.admins import admin_abilities
 from keyboards.inline.callback_data import bot_functionality
-from loader import dp
+from loader import dp, db, bot
 
 
 @dp.callback_query_handler(bot_functionality.filter(functionality_name="show_admin_panel"))
-async def show_admin_panel(call: types.CallbackQuery, callback_data: dict):
-    await call.message.answer("Админка:", reply_markup=admin_abilities)
+async def show_admin_panel(call: types.CallbackQuery):
+    await call.message.edit_text("Админка:")
+    await call.message.edit_reply_markup(admin_abilities)
+
+
+@dp.callback_query_handler(bot_functionality.filter(functionality_name="show_users"))
+async def show_users(call: types.CallbackQuery):
+    users = await db.select_all_users()
+    message_text = "Пользователи, зарегистированные в боте:\n"
+    for user in users:
+        message_text += f"{user['id']}) {user['full_name']} (@{user['username']}), был приглашен "
+        referrer_id = user['referrer_id']
+        if referrer_id == "-1001594274069":
+            message_text += "из канала"
+        else:
+            referrer = await bot.get_chat_member(referrer_id, referrer_id)
+            referrer_username = referrer['user']['username']
+            message_text += f"юзером @{referrer_username}"
+        message_text += "\n"
+    await call.message.answer(message_text)
--- keyboards/inline/admins.py
+++ keyboards/inline/admins.py
@@ -1,11 +1,14 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+from keyboards.inline.callback_data import bot_functionality
 
 admin_abilities = InlineKeyboardMarkup(inline_keyboard=[
     [
-        InlineKeyboardButton(text="Пользователи", callback_data="show_users")
+        InlineKeyboardButton(text="Пользователи",
+                             callback_data=bot_functionality.new("show_users"))
     ],
     [
-        InlineKeyboardButton(text="Добавить товар", callback_data="add_product")
+        InlineKeyboardButton(text="Добавить товар",
+                             callback_data=bot_functionality.new("add_product"))
     ]
 ])
